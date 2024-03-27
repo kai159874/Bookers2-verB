a990f370b459091421f30793dff3043f87ee44bb
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-03-27 08:08:00 +0000

    [add] create_counter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+    today = Date.today
+    yesterday = today - 1
+    current_week = ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)
+    prev_week = ((Time.current.at_end_of_day - 13.day).at_beginning_of_day)..(Time.current.at_end_of_day - 6.day)
+    @book_count_day = @books.where(created_at: today.all_day).count
+    @book_count_yesterday = @books.where(created_at: yesterday.all_day).count
+    @book_count_current_week = @books.where(created_at: current_week).count
+    @book_count_prev_week = @books.where(created_at: prev_week).count
+    if @book_count_day != 0 && @book_count_yesterday != 0
+      @book_count_ratio_day = (@book_count_day / @book_count_yesterday) * 100
+    else
+      @book_count_retio_day = nil
+    end
+    if @book_count_current_week != 0 && @book_count_prev_week != 0
+      @book_count_ratio_week = (@book_count_current_week / @book_count_prev_week) * 100
+    else
+      @book_count_retio_day = nil
+    end
   end
 
   def index
