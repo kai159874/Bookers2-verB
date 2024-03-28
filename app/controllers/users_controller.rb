58ce27907e6e595bf3a810ebad9fd10498a4ec4a
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-03-28 04:15:55 +0000

    [add] books_count_chart

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,27 @@ class UsersController < ApplicationController
     @books = @user.books
     @book = Book.new
     @book_count_day = @books.created_today.count
-    @book_count_yesterday = @books.created_yesterday.count
-    @book_count_this_week = @books.created_this_week.count
-    @book_count_last_week = @books.created_last_week.count
-    if @book_count_day != 0 && @book_count_yesterday != 0
-      @book_count_ratio_day = (@book_count_day / @book_count_yesterday) * 100
-    else
-      @book_count_ratio_day = "---"
-    end
-    if @book_count_this_week != 0 && @book_count_last_week != 0
-      @book_count_ratio_week = (@book_count_this_week / @book_count_last_week) * 100
-    else
-      @book_count_ratio_week = "---"
-    end
+    @book_count_1day_ago = @books.created_1day_ago.count
+    @book_count_2day_ago = @books.created_2day_ago.count
+    @book_count_3day_ago = @books.created_3day_ago.count
+    @book_count_4day_ago = @books.created_4day_ago.count
+    @book_count_5day_ago = @books.created_5day_ago.count
+    @book_count_6day_ago = @books.created_6day_ago.count
+
+    # @book_count_this_week = @books.created_this_week.count
+    # @book_count_last_week = @books.created_last_week.count
+    # if @book_count_day != 0 && @book_count_yesterday != 0
+    #   @book_count_ratio_day = (@book_count_day / @book_count_yesterday) * 100
+    # else
+    #   @book_count_ratio_day = "---"
+    # end
+    # if @book_count_this_week != 0 && @book_count_last_week != 0
+    #   @book_count_ratio_week = (@book_count_this_week / @book_count_last_week) * 100
+    # else
+    #   @book_count_ratio_week = "---"
+    # end
+
+    @days_record = @books.group("strftime(created_at,'%Y-%m-%d')").count
   end
 
   def index
